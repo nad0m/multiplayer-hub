cf582f6fa2f3c557c9ce50c7d91f3904fc447aaf
Adam Nguyen <adamdnguyen10@gmail.com>
2020-06-07 16:28:06 -0700

    feat: add some styling

--- src/client/pages/landing/LoginForm.js
+++ src/client/pages/landing/LoginForm.js
@@ -1,7 +1,20 @@
 import React from 'react'
 import styled from 'styled-components'
+import { UserSolidCircle } from '@styled-icons/zondicons/UserSolidCircle'
 
 import InputField from '../../components/input/InputField'
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-family: Futura, Trebuchet MS, Arial, sans-serif;
+  color: #c0c8d1;
+  > span {
+    margin: 20px 0;
+    font-weight: bold;
+  }
+`
 
 const Form = styled.form`
   > div {
@@ -9,13 +22,32 @@ const Form = styled.form`
   }
 `
 
+const CheckLabel = styled.label`
+  font-size: 13px;
+  display: flex;
+  align-items: center;
+  > input {
+   margin: 0 5px 0 25px;
+  }
+`
+
+
 const LoginForm = () => {
 
   return (
-    <Form>
-      <InputField type="text" placeholder="E-Mail Address" />
-      <InputField type="password" placeholder="Password" />
-    </Form>
+    <Wrapper>
+      <UserSolidCircle size="100" strokeWidth="2" />
+      <span>Login below to get started.</span>
+      <Form>
+        <InputField type="text" placeholder="E-mail Address" />
+        <InputField type="password" placeholder="Password" />
+        <CheckLabel>
+          <input type="checkbox" />
+          Keep me logged in
+        </CheckLabel>
+        <input type="submit" value="Login"/> 
+      </Form>
+    </Wrapper>
   )
 }
 
